e5eb7733c5ca159b7a2f145e60d7b99ff9f690b6
Max <7087907+maxpromer@users.noreply.github.com>
2024-02-07 18:47:56 +0700

    Add support ModbusRTU sensor

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -7,6 +7,7 @@ static const char * TAG = "Sensor";
 
 // include your sensor library
 #include <Wire.h>
+#include <ModbusMaster.h>
 
 #if TEMP_HUMID_SENSOR == SHT20
 #include <SHT2x.h>
@@ -20,22 +21,28 @@ static const char * TAG = "Sensor";
 #include <ArtronShop_BH1750.h>
 #endif
 
-#define SOIL_PIN A1_PIN // A1 pin
-
 #if TEMP_HUMID_SENSOR == SHT20
 SHT2x sht;
 #elif TEMP_HUMID_SENSOR == SHT30
 ArtronShop_SHT3x sht3x(SHT30_ADDR, &Wire); // ADDR: 0 => 0x44, ADDR: 1 => 0x45
 #elif TEMP_HUMID_SENSOR == SHT45
 ArtronShop_SHT45 sht45(&Wire, 0x44); // SHT45-AD1B => 0x44
+#elif TEMP_HUMID_SENSOR == ATS_TH
+ModbusMaster ats_th;
+#elif TEMP_HUMID_SENSOR == XY_MD02
+ModbusMaster xy_md02;
 #endif
 
 #if LIGHT_SENSOR == BH1750
 ArtronShop_BH1750 bh1750(BH1750_ADDR); // Non Jump ADDR: 0x23, Jump ADDR: 0x5C
+#elif TEMP_HUMID_SENSOR == ATS_LUX
+ModbusMaster ats_lux;
 #endif
 
 void Sensor_init() { // Setup sensor (like void setup())
   Wire.begin();
+  Serial2.begin(9600, SERIAL_8N1, RS485_RX_PIN, RS485_TX_PIN);
+
 #if TEMP_HUMID_SENSOR == SHT20
   if (!sht.begin()) {
     ESP_LOGE(TAG, "SHT2x not found !");
@@ -48,12 +55,18 @@ void Sensor_init() { // Setup sensor (like void setup())
   if (!sht45.begin()) {
     ESP_LOGE(TAG, "SHT45 not found !");
   }
+#elif TEMP_HUMID_SENSOR == ATS_TH
+  ats_th.begin(ATS_TH_ID, Serial2);
+#elif TEMP_HUMID_SENSOR == XY_MD02
+  xy_md02.begin(XY_MD02_ID, Serial2);
 #endif
 
 #if LIGHT_SENSOR == BH1750
   if (!bh1750.begin()) {
     ESP_LOGE(TAG, "BH1750 not found !");
   }
+#elif LIGHT_SENSOR == ATS_LUX
+  ats_lux.begin(ATS_LUX_ID, Serial2);
 #endif
 } 
 
@@ -76,6 +89,14 @@ bool Sensor_getTemp(float * value) { // Get Temperature from sensor in °C unit
     return false;
   }
   *value = sht45.temperature();
+#elif TEMP_HUMID_SENSOR == ATS_TH
+  if (ats_th.readInputRegisters(0x0001, 1) == node.ku8MBSuccess) {
+    *value = ats_th.getResponseBuffer(0) / 10.0;
+  }
+#elif TEMP_HUMID_SENSOR == XY_MD02
+  if (ats_th.readInputRegisters(0x0001, 1) == node.ku8MBSuccess) {
+    *value = xy_md02.getResponseBuffer(0) / 10.0;
+  }
 #endif
 
   return true;
@@ -95,11 +116,18 @@ bool Sensor_getHumi(float * value) { // Get Humidity from sensor in %RH unit
     return false;
   } */
   *value = sht45.humidity();
+#elif TEMP_HUMID_SENSOR == ATS_TH
+  if (ats_th.readInputRegisters(0x0002, 1) == node.ku8MBSuccess) {
+    *value = ats_th.getResponseBuffer(0) / 10.0;
+  }
+#elif TEMP_HUMID_SENSOR == XY_MD02
+  if (ats_th.readInputRegisters(0x0002, 1) == node.ku8MBSuccess) {
+    *value = xy_md02.getResponseBuffer(0) / 10.0;
+  }
 #endif
 
   return true;
 } 
-
 
 bool Sensor_getSoil(float * value) { // Get Soil moisture from sensor in % unit
 #if SOIL_SENSOR == ANALOG_SOIL_SENSOR
@@ -118,6 +146,10 @@ bool Sensor_getLight(float * value) { // Get Light from sensor in lux uint
   if (*value < 0) {
     ESP_LOGE(TAG, "BH1750 read fail");
     return false;
+  }
+#elif TEMP_HUMID_SENSOR == XY_MD02
+  if (ats_lux.readInputRegisters(0x0001, 2) == node.ku8MBSuccess) {
+    *value = (uint32_t)((lux.getResponseBuffer(1) << 16) | lux.getResponseBuffer(0));
   }
 #endif
 
--- src/UserConfigs.h
+++ src/UserConfigs.h
@@ -11,13 +11,20 @@
 #define SHT30_ADDR (0x44) // ADDR pin jump to GND => 0x44, ADDR pin jump to VCC => 0x45
 #elif TEMP_HUMID_SENSOR == SHT45
 #define SHT45_ADDR (0x44) // SHT45-AD1B => 0x44
+#elif TEMP_HUMID_SENSOR == ATS_TH
+#define ATS_TH_ID  (1)
+#elif TEMP_HUMID_SENSOR == XY_MD02
+#define XY_MD02_ID  (1)
 #endif
 
 #if SOIL_SENSOR == ANALOG_SOIL_SENSOR
+#define SOIL_PIN        A1_PIN
 #define SOIL_ANALOG_MIN (2900)
 #define SOIL_ANALOG_MAX (1500)
 #endif
 
 #if LIGHT_SENSOR == BH1750
 #define BH1750_ADDR (0x23) // ADDR pin jump to GND => 0x23, ADDR pin jump to VCC => 0x5C
+#elif LIGHT_SENSOR == ATS_LUX
+#define ATS_LUX_ID  (1)
 #endif
